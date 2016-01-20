030d097c57f5b65d8b7e2993829b4e6ffc262d51
Tariq Islam <tariq.m.islam@gmail.com>
2016-01-20 15:20:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
  <?php 
- echo "I am VERSION 1, the initial deployment and things are great. <br><br>";
+ echo "I am VERSION 2, the next generation and things are even better than before! <br><br>";
  echo "Automated build. My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
